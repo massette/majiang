51e5a0ab5470d9621db398ab1d1ed65bf7834c29
Person's Name <artofcoded@gmail.com>
2020-01-19 15:18:06 -0500

    testing express router

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ const pages = {
 }
 const page_names = Object.keys(pages)
 
+let thing = express.Router()
+
 online = {}
 recent = {}
 
@@ -59,6 +61,12 @@ page_names.forEach((name) => {
         timed_log(`Opened: ${pages[name]}`)
     })
 })
+
+thing.get("/",(req,res) => {
+    res.send("respond with the thing")
+})
+
+app.use("/users",thing)
 
 const PORT = (process.env.PORT | 3000)
 
